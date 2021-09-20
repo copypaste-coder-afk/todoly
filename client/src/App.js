e9fa5ee2e557c3bf27d3c559b677360125beb9e0
copypaste-coder-afk <greengeeksofficial@gmail.com>
2021-09-20 17:32:26 +0500

    Done With Edit & Delete Todo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import InputTodo from './components/InputTodo.jsx'
+import ListTodos from './components/ListTodo.jsx'
 import React, { Fragment } from 'react'
 function App() {
   return (
     <Fragment>
       <div className="container">
         <InputTodo />
+        <ListTodos />
       </div>
     </Fragment>
   )
