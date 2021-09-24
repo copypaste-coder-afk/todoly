4cd7ecd8c5f42ab8df668f02dd2a1d8798b9695e
copypaste-coder-afk <greengeeksofficial@gmail.com>
2021-09-24 18:29:33 +0500

    Made The Login Page And Updated The Front-End Functionality

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const Register = ({setAuth}) => {
+    return (
+        <Fragment>
+            <h1 className="color: red">Hello World</h1>
+        </Fragment>
+    )
+}
+
+export default Register;
